d154f0b57194adbabbdcb7b1bd07565837b568f7
Thanathip Limna <boatkrap@gmail.com>
2025-02-16 22:36:18 +0700

    add script for first initial

--- first-setup.sh
+++ first-setup.sh
@@ -1,1 +1,5 @@
 ansible-playbook -u pi --ask-pass -i hosts playbook/create_ansible_user.yml 
+ansible-playbook -i hosts playbook/create_ansible_user.yml
+ansible-playbook -i hosts playbook/create_share_fs.yml
+ansible-playbook -i hosts playbook/setup_cluster.yml
+ansible-playbook -i hosts playbook/install_packages.yml
